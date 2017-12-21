80e9a109bd46f951c219721a742be22332d9d044
Anders Svensson <anders.m.sv@gmail.com>
2017-12-21 14:45:29 +0100

    "API added"

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,9 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function(req, res){
     res.json([
         {"id": 1, "firstName":"Bob", "lastname":"Smith", "email":"bob@gmail.com"},
         {"id": 2, "firstName":"Tammy", "lastname":"Norton", "email":"tnorton@yahoo.com"},
