6918eb409920c757fcc365ce2d2b92cfd987a0fa
Anders Svensson <anders.m.sv@gmail.com>
2017-12-21 13:20:58 +0100

    "Trying again"

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -16,7 +16,12 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../src/index.html'));
+    //res.sendFile(path.join(__dirname, '../src/index.html'));
+    res.json([
+        {"id": 1, "firstName":"Bob", "lastname":"Smith", "email":"bob@gmail.com"},
+        {"id": 2, "firstName":"Tammy", "lastname":"Norton", "email":"tnorton@yahoo.com"},
+        {"id": 3, "firstName":"Tina", "lastname":"Lee", "email":"lee.tina@hotmail.com"}
+    ]);
 });
 
 app.listen(port, function(err){
